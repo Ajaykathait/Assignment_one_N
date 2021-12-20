3a907bb3b1d5971a4a58bca664fed8bc810b022a
Ajay <kathaitajayk@gmail.com>
2021-12-20 17:50:13 +0530

    resolv comment

--- src/pages/public/counter/Counter.js
+++ src/pages/public/counter/Counter.js
@@ -2,20 +2,20 @@ import React, { useState } from "react";
 import "./Counter.css";
 
 export default function Counter() {
-  const [count, setcount] = useState(0);
+  const [count, setCount] = useState(0);
 
   return (
     <div className="counter">
       <button
         onClick={() => {
           count > 0
-            ? setcount((ppre) => ppre - 1)
+            ? setCount(count - 1)
             : alert("you can not decrease it after 0");
         }}>
         -
       </button>
       <h1>{count}</h1>
-      <button onClick={() => setcount((ppre) => ppre + 1)}>+</button>
+      <button onClick={() => setCount(count + 1)}>+</button>
     </div>
   );
 }
