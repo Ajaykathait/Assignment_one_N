11efc4f4fb1068cdaae70d895652cb0f38b80c0c
Ajay <kathaitajayk@gmail.com>
2021-12-23 17:29:24 +0530

    signUp Page

--- src/layout/publicLayout.js
+++ src/layout/publicLayout.js
@@ -2,21 +2,23 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 // import Counter from "@pages/public/counter/Counter";
-import ReduxCounter from "@pages/public/counter-redux/ReduxCounter";
+// import ReduxCounter from "@pages/public/counter-redux/ReduxCounter";
+import SignUp from "@pages/public/signup";
 
 export default function PublicLayout({ children }) {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
         <Grid item xs={7}>
-          <h2>Some placeholder</h2>
+          {/* <h2>Some placeholder</h2> */}
         </Grid>
         <Grid item xs={5}>
           {children}
         </Grid>
       </Grid>
       {/* <Counter /> */}
-      <ReduxCounter />
+      {/* <ReduxCounter /> */}
+      <SignUp />
     </Box>
   );
 }
--- src/pages/public/signup/index.js
+++ src/pages/public/signup/index.js
@@ -1,7 +1,101 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./index.scss";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState([]);
+  const [rePass, setRePass] = useState([]);
+  const localStorageData = localStorage.getItem("user");
+  const [Array, setArray] = useState(
+    localStorageData ? JSON.parse(localStorageData) : []
+  );
+
+  const userDetails = {
+    Name: name,
+    Email: email,
+    Password: password
+  };
+  const submitForm = (event) => {
+    password == rePass
+      ? (setArray([...Array, userDetails]), event.preventDefault())
+      : (event.preventDefault(),
+        alert("!!! Password Field and Re-Enter Password Field should match"));
+  };
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(Array));
+  }, [Array]);
+  return (
+    <div>
+      <div className="SignUp-page">
+        <form onSubmit={submitForm}>
+          <div className="single-input">
+            <label htmlFor="">Full Name</label>
+            <input
+              type="text"
+              placeholder="Name"
+              onChange={(event) => {
+                setName(event.target.value);
+              }}
+              required
+            />
+          </div>
+          <div className="single-input">
+            <label htmlFor="">Email</label>
+            <input
+              type="mail"
+              placeholder="Email"
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
+              required
+            />
+          </div>
+          <div className="single-input">
+            <label htmlFor="">Password</label>
+            <input
+              type="password"
+              placeholder="Password"
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+              required
+            />
+          </div>
+          <div className="single-input">
+            <label htmlFor="">Re-Enter Password</label>
+            <input
+              type="password"
+              placeholder="Re-Enter Password"
+              onChange={(event) => {
+                setRePass(event.target.value);
+              }}
+              required
+            />
+          </div>
+
+          <div>
+            {password.length <= rePass.length && password.length != 0 ? (
+              password == rePass ? (
+                <p style={{ color: "green" }}>Password is Matched</p>
+              ) : (
+                <p style={{ color: "red" }}>!! Password doesn&apos;t Match</p>
+              )
+            ) : null}
+          </div>
+
+          <p style={{ display: "none" }}>
+            {name} {email} {password} {rePass}
+          </p>
+
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
