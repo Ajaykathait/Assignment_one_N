8d0101af9c2694ffa894e325dd34235c32acabfe
Ajay <kathaitajayk@gmail.com>
2021-12-29 13:48:57 +0530

    Gallery modification

--- src/pages/private/dashboard/index.js
+++ src/pages/private/dashboard/index.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import { AppDispatcher } from "@redux/";
 import Gallery from "@pages/public/imgae-gallery/Gallery";
 
 function Dashboard() {
+  const [toggle, setToggle] = useState(false)
   const showGallery = () => {
-    console.log('a');
+    toggle == false ? setToggle(true) : setToggle(false)
   }
+
   return (
     <div>
       <h2>Dashboard</h2>
-      <Button variant="contained" onClick={showGallery}>Gallery</Button>
+      <Button variant="contained" onClick={showGallery}> {toggle == true ? "Hide Gallery" : 'Show Gallery'}</Button>
       <Button style={{ marginLeft: '10px' }} variant="contained" onClick={() => { AppDispatcher.setUserLoggedOut() }}>LogOut</Button>
-      <Gallery />
+      {toggle == true ? <Gallery /> : null}
     </div>
   );
 }
