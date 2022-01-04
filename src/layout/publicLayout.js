99ecfea28e82fd395bfd08d3d266e49b341bcf89
Ajay Kathait <kathaitjayk@gmail.com>
2022-01-04 17:47:52 +0530

    common header for public layout

--- src/layout/publicLayout.js
+++ src/layout/publicLayout.js
@@ -1,17 +1,11 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-
+import Header from "@components/header/Header";
 
 export default function PublicLayout({ children }) {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={1}>
-        </Grid>
-        <Grid item xs={5}>
-        </Grid>
-      </Grid>
+      <Header />
       {children}
     </Box>
   );
