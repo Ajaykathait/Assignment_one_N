ec5a7b11c15f95bddb79a7a96d9c30aac617adcf
Ajay Kathait <kathaitjayk@gmail.com>
2022-01-04 17:49:10 +0530

    css fixes for Counter Component

--- src/pages/public/counter-redux/ReduxCounter.js
+++ src/pages/public/counter-redux/ReduxCounter.js
@@ -2,13 +2,15 @@ import React from "react";
 
 import dispatch from "@redux/dispatchers/appDispatcher";
 import { useSelector } from "react-redux";
+import styles from "../counter/Counter.module.css";
 
 export default function Counter() {
   const myValue = useSelector((state) => state.UpdateNum);
 
   return (
-    <div className="counter">
+    <div className={styles.counter}>
       <button
+        className={styles.btn}
         onClick={() => {
           dispatch.updateDec();
         }}>
@@ -16,6 +18,7 @@ export default function Counter() {
       </button>
       <h1>{myValue}</h1>
       <button
+        className={styles.btn}
         onClick={() => {
           dispatch.updateInc();
         }}>
--- src/router/routes/publicRoutes.js
+++ src/router/routes/publicRoutes.js
@@ -4,10 +4,12 @@ import Login from "@pages/public/login";
 import SignUp from "@pages/public/signup";
 import Counter from "@pages/public/counter/Counter";
 import Gallery from "@pages/public/imgae-gallery/Gallery";
+import ReduxCounter from "@pages/public/counter-redux/ReduxCounter";
 
 export const PublicRoutes = [
   { path: "/auth/login", exact: true, component: Login },
   { path: "/auth/signup", exact: true, component: SignUp },
   { path: "/auth/Counter", exact: true, component: Counter },
-  { path: '/auth/gallery', exact: true, component: Gallery }
+  { path: "/auth/redux-counter", exact: true, component: ReduxCounter },
+  { path: "/auth/gallery", exact: true, component: Gallery }
 ];
